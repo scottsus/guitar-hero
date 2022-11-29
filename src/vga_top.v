b2d238e04e23b4451c86342c8bdb7e6a7797aa55
hnrose2223 <hnrose@usc.edu>
2022-11-28 16:04:42 -0800

    Update vga_top.v

--- src/vga_top.v
+++ src/vga_top.v
@@ -24,7 +24,7 @@
 module vga_top(
 	input ClkPort,
 	input BtnC,
-	input BtnU,
+        input BtnU, BtnD, BtnL, BtnR,
 	
 	//VGA signal
 	output hSync, vSync,
@@ -44,7 +44,7 @@ module vga_top(
 	wire [3:0] anode;
 	wire [11:0] rgb;
 	display_controller dc(.clk(ClkPort), .hSync(hSync), .vSync(vSync), .bright(bright), .hCount(hc), .vCount(vc));
-	vga_bitchange vbc(.clk(ClkPort), .bright(bright), .button(BtnU), .hCount(hc), .vCount(vc), .rgb(rgb), .score(score));
+        vga_bitchange vbc(.clk(ClkPort), .bright(bright), .button(BtnU), .button(BtnD), .button(BtnR), .button(BtnL), .hCount(hc), .vCount(vc), .rgb(rgb), .score(score));
 	counter cnt(.clk(ClkPort), .displayNumber(score), .anode(anode), .ssdOut(ssdOut));
 	
 	assign Dp = 1;
